26e32d61f84a796bc07e00aef2fda9c193ed8cb8
Ángel Sánchez <mektigboy@protonmail.com>
2022-05-29 00:14:39 -0500

    Create & update Constants.sol

---
+++ Constants.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.14;
+
+contract Constants {
+  address public constant MY_ADDRESS = 0x0000000000000000000000000000000000000000;
+  uint public constant MY_UINT = 123;
+}
+
+contract Var {
+  address public MY_ADDRESS = 0x0000000000000000000000000000000000000000;
+}
